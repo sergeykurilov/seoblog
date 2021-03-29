c7ceba5d9afd78cabd95a99eaad05789560c5311
Sergey <kurilovsergey15@gmail.com>
2021-03-29 21:27:02 +0300

    header

--- frontend/Components/Header.jsx
+++ frontend/Components/Header.jsx
@@ -19,7 +19,7 @@ const Example = (props) => {
     const [isOpen, setIsOpen] = useState(false);
 
     const toggle = () => setIsOpen(!isOpen);
-//jhjjhjhjhjhjфывфыв
+//222sssss
     return (
         <div>
             <Navbar color="light" light expand="md">
