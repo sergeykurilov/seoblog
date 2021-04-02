8543b92f7fbfea6d58a1d16ac2afcd386ba2d954
Sergey <kurilovsergey15@gmail.com>
2021-04-03 00:48:28 +0300

    blog api

---
+++ backend/models/blog.js
@@ -0,0 +1,50 @@
+const mongoose = require("mongoose")
+const crypto = require("crypto")
+const {ObjectId} = require("mongoose")
+
+const blogSchema = new mongoose.Schema({
+        title: {
+            type: String,
+            trim: true,
+            required: true,
+            min: 3,
+            max: 160,
+            unique: true,
+        },
+        slug: {
+            type: String,
+            unique: true,
+            index: true,
+        },
+        body: {
+            type: {},
+            required: true,
+            min: 20,
+            max: 2000000,
+        },
+        mDesc: {
+            type: String,
+        },
+        mTitle: {
+            type: String,
+        },
+        exerpt: {
+            type: String,
+            max: 1000,
+        },
+        photo: {
+            data: Buffer,
+            contentType: String
+        },
+        categories: [{type: ObjectId, ref: "Category", required: true}],
+        tag: [{type: ObjectId, ref: "Tag", required: true}],
+        postedBy: {
+            type: ObjectId,
+            ref: "User"
+        }
+    }, {timestamps: true})
+
+
+
+
+module.exports = mongoose.model("Blog", blogSchema)
