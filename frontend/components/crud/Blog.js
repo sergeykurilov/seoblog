b62507f572b72976a047e6e44404309f751e987c
Sergey <kurilovsergey15@gmail.com>
2021-04-20 21:46:52 +0300

    blog

--- frontend/components/crud/Blog.js
+++ frontend/components/crud/Blog.js
@@ -112,7 +112,7 @@ const CreateBlog = () => {
                 </form>
             )
         }
-
+///askjdnaj
 
         const handleToggleCategories = (c) => () => {
             setValues({...values, error: ""})
