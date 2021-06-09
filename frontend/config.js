a1479cea43085ab35eebd86e840051b7129e878e
Sergey <kurilovsergey15@gmail.com>
2021-06-09 23:46:05 +0300

    google analytics

--- frontend/config.js
+++ frontend/config.js
@@ -1,7 +1,9 @@
 import getConfig from 'next/config';
 const {publicRuntimeConfig} = getConfig();
 
-export const API = publicRuntimeConfig.PRODUCTION ? "https://seoblog.com" : "http://localhost:8000/api";
+
+
+export const API = publicRuntimeConfig.PRODUCTION ? publicRuntimeConfig.PRODUCTION : publicRuntimeConfig.APP_DEVELOPMENT;
 export const APP_NAME = publicRuntimeConfig.APP_NAME;
 
 export const DOMAIN = publicRuntimeConfig.PRODUCTION
--- frontend/pages/_document.jsx
+++ frontend/pages/_document.jsx
@@ -1,8 +1,24 @@
 import Document, {Html, Head, Main, NextScript} from 'next/document'
+import getConfig from "next/config";
 
+const {publicRuntimeConfig} = getConfig();
 
 
 class MyDocument extends Document {
+
+    setGoogleTags() {
+        if (publicRuntimeConfig.PRODUCTION) {
+            return {
+                html: `window.dataLayer = window.dataLayer || [];
+                     function gtag(){dataLayer.push(arguments);} 
+                     gtag('js', new Date());
+                     gtag('config', 'UA-179619384-1');
+                     `
+            }
+        }
+    }
+
+
     render() {
         return (
             <Html lang="en">
@@ -14,14 +30,20 @@ class MyDocument extends Document {
                     <link rel="stylesheet"
                           href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.6.0/css/bootstrap.min.css"/>
                     <link rel="stylesheet" href="/static/css/style.css"/>
-                    <link rel="icon" href="/favicon.ico" />
-                    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/2.1.2/tailwind.min.css" />
-                    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/2.1.2/base.min.css" />
-                    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/2.1.2/components.css" />
-                    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/2.1.2/tailwind-dark.min.css" />
-                    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/2.1.2/utilities.min.css" />
+                    <link rel="icon" href="/favicon.ico"/>
+                    <link rel="stylesheet"
+                          href="https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/2.1.2/tailwind.min.css"/>
+                    <link rel="stylesheet"
+                          href="https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/2.1.2/base.min.css"/>
+                    <link rel="stylesheet"
+                          href="https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/2.1.2/components.css"/>
+                    <link rel="stylesheet"
+                          href="https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/2.1.2/tailwind-dark.min.css"/>
+                    <link rel="stylesheet"
+                          href="https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/2.1.2/utilities.min.css"/>
                     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/quill/1.3.7/quill.snow.css"/>
                     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/nprogress/0.2.0/nprogress.css"/>
+                    <script dangerouslySetInnerHTML={this.setGoogleTags()}></script>
                 </Head>
                 <body>
                 <Main/>
